5a05caa36c848da188c3c7779f37f3d2fdd0edf6
Anthony Valenzuela <aovalenz@uci.edu>
2022-02-10 21:49:47 -0800

    changeed structure of index file

--- indexer.py
+++ indexer.py
@@ -1,7 +1,7 @@
 import json
 import nltk
 
-def createIndex(tokens, html):
+def createIndex(tokens: [str], html: int):
     """creates the index of all tokens with list of postings
     JSON file structure:
     token: [ [html,frequency] ]
@@ -15,21 +15,14 @@ def createIndex(tokens, html):
         index = json.loads(jsonFile)
         # go through all tokens and add to the indexer
         for token in freqDict:
-            postings = [[html, 1]]
+            postings = [{html: [0]}, 0]
             if token in index:
                 # list of postings
                 postings = index[token]
-                # go through list and find the right html page
-                updatedFrequency = 0
-                for posting in postings:
-                    if posting[0] == html:
-                        updatedFrequency = posting[1] + freqDict[token]
-                        break
-                # add this to list of postings
-                data = [html, updatedFrequency]
-                # update list of postings
-                postings.append(data)
-
+                # add key into document dictionary
+                frequency = postings[0][html]
+                updatedFrequency = frequency + 1
+                postings[0][html][0] = updatedFrequency # this will have an updated list
             index[token] = postings
             # start from beginning of file
             jsonFile.seek(0)
