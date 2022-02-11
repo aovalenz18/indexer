ac90c8bc316e56a2a5bed30322bd19ef85393e29
kazeemsalaam <kosalaam@uci.edu>
2022-02-11 11:11:33 -0800

    updated tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -3,24 +3,23 @@ import json
 import re
 from bs4 import BeautifulSoup
 
-# kazee
+# kazeem
 def openHtml(file):
     '''Open files in DEV folder, go through all JSON files and gather text and tokens'''
     'returns a list of all tokens in the html page '
     newFile = open(file)
-    fileData = json.load(newFile, "r") #file data is now a dictionary
+    fileData = json.load(newFile) #file data is now a dictionary
 
     soup = BeautifulSoup(fileData["content"], 'html.parser')  # creates the soup object to extract all the text
 
     fullText = soup.get_text().lower() # gets all text from the document in one string
 
-    #tokenizes strings and separates hyphenated words
-    tokenizer = re.findall(r"[a-zA-Z0-9]+", fullText)
-    tokenizer = tokenizer.replace("-", " ")
+    #tokenizes strings
+    tokens = re.findall(r"[a-zA-Z0-9]+", fullText)
 
     # create list of tokens
-    filteredTokens = [word for word in tokenizer.split()]
-    return filteredTokens
+    #filteredTokens = [word for word in tokenizer.split()]
+    return tokens
 
 
 def parseTokens(tokens):
