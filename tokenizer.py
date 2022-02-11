72d15efc86479d0b8e13a710f30bef54f2374b6e
kazeemsalaam <kosalaam@uci.edu>
2022-02-11 10:26:18 -0800

    openHTML func in tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -1,10 +1,26 @@
 from nltk.stem import PorterStemmer
+import json
+import re
+from bs4 import BeautifulSoup
 
-
+# kazee
 def openHtml(file):
     '''Open files in DEV folder, go through all JSON files and gather text and tokens'''
     'returns a list of all tokens in the html page '
-    pass
+    newFile = open(file)
+    fileData = json.load(newFile, "r") #file data is now a dictionary
+
+    soup = BeautifulSoup(fileData["content"], 'html.parser')  # creates the soup object to extract all the text
+
+    fullText = soup.get_text().lower() # gets all text from the document in one string
+
+    #tokenizes strings and separates hyphenated words
+    tokenizer = re.findall(r"[a-zA-Z0-9]+", fullText)
+    tokenizer = tokenizer.replace("-", " ")
+
+    # create list of tokens
+    filteredTokens = [word for word in tokenizer.split()]
+    return filteredTokens
 
 
 def parseTokens(tokens):
