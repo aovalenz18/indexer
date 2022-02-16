0507cf3b270bc57b28322e3b801d604e811b88c7
Anthony Valenzuela <aovalenz@uci.edu>
2022-02-15 18:29:09 -0800

    completed the create matrix function

--- query.py
+++ query.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def search(tokens: list):
     # Kazeem
     """
@@ -15,7 +18,27 @@ def createMatrix(tokenDict: dict):
     Will parse through given dictionary, find all pages that the words go in and place a 1 wherever the documents
     appear
     """
-    pass
+    # find the number of total pages for all tokens and map them to an index
+    pageMapping = {}
+    indexCounter = 0
+    for key in tokenDict:
+        pages = tokenDict[key][0].keys()
+        for page in pages:
+            if page not in pageMapping:
+                pageMapping[page] = indexCounter
+                indexCounter += 1
+
+    matrixShape = (len(tokenDict), len(pageMapping))
+    matrix = np.zeros(matrixShape)
+
+    i = 0 # counter for each token in the dictionary
+    for token in tokenDict:
+        pages = list(tokenDict[token][0].keys())
+        for page in pages:
+            matrix[i, pageMapping[page]] = 1
+        i += 1
+
+    return matrix
 
 
 def matrixResults(matrix: [list], tokenDict: dict):
@@ -28,3 +51,64 @@ def matrixResults(matrix: [list], tokenDict: dict):
     and navigate through dictionary to get the corresponding file path name
     '''
     pass
+
+if __name__ == "__main__":
+
+    adict = {"real": [
+        {
+            "9": [
+                2
+            ],
+            "8": [
+                2
+            ]
+        },
+        0
+    ],
+    "time": [
+        {
+            "9": [
+                2
+            ],
+            "1": [
+                2
+            ]
+        },
+        0
+    ],
+    "dre": [
+        {
+            "9": [
+                2
+            ],
+            "4": [
+                2
+            ]
+        },
+        0
+    ],
+    "feedback": [
+        {
+            "9": [
+                2
+            ],
+            "7": [
+                2
+            ]
+        },
+        0
+    ],
+    "if": [
+        {
+            "9": [
+                2
+            ],
+            "5": [
+                2
+            ]
+        },
+        0
+    ],}
+
+    booleanMatrix = createMatrix(adict)
+    print(booleanMatrix)
