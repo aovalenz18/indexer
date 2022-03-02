24e28fac8f9f6ceb5d61bb99cf06ec4367a872ea
Anthony Valenzuela <aovalenz@uci.edu>
2022-03-01 21:29:27 -0800

    made changes to search to read from text file and create matrix to include tfidf

--- indexer.py
+++ indexer.py
@@ -6,7 +6,7 @@ import os
 from pathlib import Path
 
 
-# Holds on to current indexs
+# Holds on to current indexes
 globalIndex = {}
 
 # Stores temporary document ID
--- querymain.py
+++ querymain.py
@@ -1,17 +1,41 @@
 from interface import *
 from query import *
+import json
+
+gIndex = {}
+gFile = None
+
+
+def openFiles():
+    """
+    :return: index dictionary and file index objects
+    This function is going to open up the lineNums.json
+    """
+    with open('data.json') as f:
+        data = json.load(f)
+    # this is going to contain the index of the tokens
+
+    file = open("txtIndex.txt", "r")
+
+    return data, file
+
+
+def closeFile(file):
+    file.close()
 
 
 '''Main part of query program, have terminal interface to access indexer
 Get user input and call outside functions to get the query results'''
-if __name__=="__main__":
+if __name__ == "__main__":
+
+    gIndex, gFile = openFiles()
 
     print("Assignment #3 - Search Engine")
     userInput = getUserInput()
 
     while userInput != "q":
         # Add this part when the functions gets done
-        
+
         # Get a smaller dictionary from the indexer from the result of the userInput
         indexDict = search(userInput)
 
@@ -24,19 +48,18 @@ if __name__=="__main__":
         # Output the documentList
         # printResult(documentList)
         print(documentList)
-        
-        
-        
 
         # Used to see what the parsed userInput look like
         print(userInput)
 
         # For testing search
-        #indexDict = search(userInput)
-        #print(indexDict)
+        # indexDict = search(userInput)
+        # print(indexDict)
 
         userInput = getUserInput()
 
+    closeFile(gFile)
     print("\nClosing the program")
+
 
     pass
